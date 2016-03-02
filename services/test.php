f1abe1b1ace9533b1ed05068f2a0a46ffc6ac2c5
ruchira22 <ruchiraupadhyay.bhumca2014@gmail.com>
2016-03-02 14:01:49 +0530

    cleaning sprint7

--- services/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = mysqli_connect("localhost","root","root1234","magzhub"); //keep your db name
-$sql = "SELECT issueThumbnail FROM tempissueupload WHERE issueId =31";
-$sth = $db->query($sql);
-$result=mysqli_fetch_array($sth);
-echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['issueThumbnail'] ).'"/>';
-?>
